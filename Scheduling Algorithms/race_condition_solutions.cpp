f32b447dd420bc14d5aad83b597efbb7d302ada0
Aditya Keshari <keshariaditya90@gmail.com>
2022-10-12 20:30:26 +0530

    nf

--- Scheduling Algorithms/race_condition_solutions.cpp
+++ Scheduling Algorithms/race_condition_solutions.cpp
@@ -63,6 +63,7 @@ void taskB() {
 
 /*
 
+
 // peterson's solution 
 // (Code Not working correctly)
 
@@ -72,14 +73,21 @@ vector<bool> flag(2,false);
 
 void taskA() {
     while(1) {
+        cout << "TASK-A" <<endl;         // to check the order in which the tasks are executed     
         flag[0] = true;
         turn = 1;
-        while(turn == 1 and turn != 0);
-        cout << "TASK-A" <<endl;         // to check the order in which the tasks are executed     
+
+        while(turn == 1 and flag[1] == true) {
+            cout << " Trapped in task A " << endl;
+        }
+
         for(int i = 0; i < 1e6; i++) {
             cnt++;
         }
         flag[0] = false;
+
+        cout << "taskA completed " << endl;
+
         break;
     }
 }
@@ -87,17 +95,41 @@ void taskA() {
 
 void taskB() {
     while(1) {
+        cout << "TASK-B" <<endl;    
         flag[1] = true;
         turn = 0;
-        while(turn == 0 and flag[0] == true);
-        cout << "TASK-B" <<endl;    
+
+        while(turn == 0 and flag[0] == true)
+            cout << " Trapped in task B " << endl;
+        
+
         for(int i = 0; i < 1e6; i++) {
             cnt++;
         } 
         flag[1] = false;
+
+        cout << "taskB completed " << endl;
+        
         break;
     }
 }
+
+
+
+
+
+int main() {
+    thread t1(taskA), t2(taskB);  // thread t1 is defined first so, may be it is initilized the first.
+
+    t2.join();
+    t1.join();  // doesn't let main thread to exit unless t1 thread completes execution 
+
+    cout << cnt << endl;
+    return 0;
+}
+
+
+
 
 */ 
 
